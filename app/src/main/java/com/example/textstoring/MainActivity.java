cd6d161003f20f5aadeb961c468ce702e752e72e
ART2010311 <induthakur78687@gmail.com>
2023-09-30 11:10:58 +0545

    updated with load button

--- app/src/main/java/com/example/textstoring/MainActivity.java
+++ app/src/main/java/com/example/textstoring/MainActivity.java
@@ -8,11 +8,17 @@ import android.widget.Toast;
 import android.widget.Button;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
     EditText mEditText;
-    Button btn;
+    Button btn,btnLoad;
+
     private static final String FILE_NAME="example.txt";
 
     @Override
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mEditText=findViewById(R.id.editTextText);
         btn=findViewById(R.id.Save);
+        btnLoad=findViewById(R.id.Load);
+
         btn.setOnClickListener(view -> {
             String text=mEditText.getText().toString();
             FileOutputStream fileOutputStream=null;
@@ -36,6 +44,34 @@ public class MainActivity extends AppCompatActivity {
                 if(fileOutputStream !=null){
                     try{
                         fileOutputStream.close();
+
+                    }catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        btnLoad.setOnClickListener(view -> {
+
+            FileInputStream fileInputStream=null;
+            try{
+                fileInputStream=openFileInput(FILE_NAME);
+
+                InputStreamReader inputStreamReader=new InputStreamReader(fileInputStream);
+                BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+                StringBuilder stringBuilder=new StringBuilder();
+                String text;
+                while ((text = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(text).append("\n");
+                }
+                mEditText.setText(stringBuilder.toString());
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }finally{
+                if(fileInputStream !=null){
+                    try{
+                        fileInputStream.close();
 
                     }catch(IOException e) {
                         e.printStackTrace();
